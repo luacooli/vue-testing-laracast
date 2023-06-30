e3f7e3543c3d62502d1d8fe3cc997b38cc3ba81d
Luana <lcorreia@vntrx.com>
2023-06-29 22:51:18 -0300

    lesson: reminder test

---
+++ src/components/Reminders.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <input class="new-reminder" v-model="newReminder" />
+    <button @click="add">Add reminder</button>
+
+    <ul v-if="reminders.length">
+      <li v-for="reminder in reminders" :key="reminder">
+        {{ reminder }}
+
+        <button class="remove" @click="remove(reminder)">Remove</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line
+  name: "Reminders",
+
+  data() {
+    return {
+      newReminder: "",
+      reminders: [],
+    };
+  },
+
+  methods: {
+    add() {
+      this.reminders.push(this.newReminder);
+      this.newReminder = "";
+    },
+    remove(reminder) {
+      this.reminders.splice(reminder, 1);
+    },
+  },
+};
+</script>
---
+++ tests/unit/Reminders.spec.js
@@ -0,0 +1,44 @@
+import { mount } from "@vue/test-utils";
+import Reminders from "@/components/Reminders.vue";
+
+describe("Reminders", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(Reminders);
+  });
+
+  it("hides the reminders list if there are none", () => {
+    expect(wrapper.contains("ul")).toBe(false);
+  });
+
+  it("can add reminders", () => {
+    addReminder("Go to the store");
+
+    expect(remindersList()).toContain("Go to the store");
+  });
+
+  it("can delete any reminder", () => {
+    addReminder("Go to the store");
+    addReminder("Finish screencast");
+
+    let deleteButton = wrapper.find("ul > li:first-child .remove");
+
+    deleteButton.trigger("click");
+
+    expect(remindersList()).not.toContain("Go to the store");
+  });
+
+  function addReminder(body) {
+    let newReminder = wrapper.find(".new-reminder");
+
+    newReminder.element.value = body;
+    newReminder.trigger("input");
+
+    wrapper.find("button").trigger("click");
+  }
+
+  function remindersList() {
+    return wrapper.find("ul").text();
+  }
+});
